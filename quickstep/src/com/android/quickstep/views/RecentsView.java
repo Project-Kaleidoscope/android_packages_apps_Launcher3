bb5b7cbdab0af2faee793332e0c17c5f16bf0a29
LibXZR <i@xzr.moe>
2022-02-27 00:01:04 +0800

    Fix crash when page scroll is not generated
    In some case, e.g. opening apps that only support landscape mode for
    the first time, possibilities exist that the taskview is added before
    generating the page scroll. This could lead to IndexOutOfBoundsException
    and crash the launcher.
    
    Change-Id: I27badfe6673a3a806dcebfd0c646e949eb49b46e
    Signed-off-by: LibXZR <i@xzr.moe>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3911,7 +3911,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
                                 && mOrientationState.getTouchRotation() != ROTATION_180;
-        int childCount = getChildCount();
+        int childCount = mPageScrolls.length;
         int curScroll = isInLandscape ? getScrollY() : getScrollX();
 
         for (int i = 0; i < childCount; i++) {
